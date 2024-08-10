36620435537f08b36893b6bfe30ee7b833290096
Lay182 <jinwoo9288@gmail.com>
2024-08-10 22:53:05 +0900

    two commit

--- rest-api/src/main/java/com/example/rest_api/model/UserRequest.java
+++ rest-api/src/main/java/com/example/rest_api/model/UserRequest.java
@@ -1,22 +1,26 @@
 package com.example.rest_api.model;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequest {
+
     private String userName;
 
     private Integer userAge;
-
+    @JsonProperty("EMAIL")
     private String email;
 
     private Boolean isKorean;
-
 }
--- rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.rest_api;
 
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+//		var user = new UserRequest("홍길동",10,"hong@gmail.com",true);
+//		System.out.println(user);
+//		user.setUserName("홍길동");
+//		user.setUserAge(10);
+//		user.setEmail("hong@gmail.com");
+//		user.setIsKorean(true);
+
+		var json = "{\"user_names\":\"홍길동\",\"user_age\":10,\"email\":\"hong@gmail.com\",\"is_korean\":true}";
+		System.out.println(json);
+
+		var dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println(dto);
+
 	}
 
 }
