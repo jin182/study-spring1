513e7b394cac97eaf9eae3cb1b9bda8e92edaeda
Lay182 <jinwoo9288@gmail.com>
2024-08-21 17:05:36 +0900

    study filter1

---
+++ part1/filter/src/main/java/com/example/filter/filter/LoggerFilter.java
@@ -0,0 +1,37 @@
+package com.example.filter.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //진입전
+        log.info(">>>>>>> 진입");
+
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        chain.doFilter(req, res );
+        var reqJson = new String(req.getContentAsByteArray());
+        log.info("req :  {}", reqJson);
+        var resJson = new String(res.getContentAsByteArray());
+        log.info("res : {}", resJson);
+        log.info("<<<<<<< 리턴");
+        //진입후
+        res.copyBodyToResponse();
+    }
+}
